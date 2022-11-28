1bb181f9a825caa3d114fec8f4d81c5303d97b16
lumingyu0423 <lumingyu0423@gmail.com>
2022-11-28 08:56:33 +0800

    support android10 ,test from HTC 5G HUB.

--- HTC_OTA_Downloader.py
+++ HTC_OTA_Downloader.py
@@ -37,19 +37,48 @@ def checkin():
         'x-active-g': 'DivadGS38Omatump76'
     }
     queryjson = {
-        'imei': IMEI,
+        "country":"US",
+        "adminArea":"",
+        "locality":"",
+        "zip":"",
+        "sub_locality":"",
+        "statusCode":7,
+        "digest":"",
+        "locale":"en_US",
+        "imei":"353002100146660",
+        "timeZone":"GMT-08:00",
+        "mFlag":"1",
+        "aaReport":"com",
+        "currentOS":"aos",
+        "mAct":"NA",
+        "mNos":"NA,NA",
+        "sn_digest":"0",
+        "model_number": model,
+        "last_checkin_msec":"0",
         'timeStamp': timestamp,
-        'model_number': model,
         'checkin': {
             'build': {
-                'serialno': SN,
+                "carrier":"htc",
+                "product":"htc_rtx",
+                "revision":"0",
+                "serialno":"FA0611N00326",
+                "bootloader":"",
+                "radio":"",
+                "changelist":"",
+                "build_type":"user",
+                'build_taskid': taskid,
                 'firmware_version': version
             },
-            'client_version': 'A8.0(M)',
-            'cid': cidnum,
-            'checkin_type': 'Manual'
+            'client_version': 'A10.0(P)',
+            "cid": cidnum,
+            "checkin_type":"Manual", # Auto
+            "mcc_mnc":"",
+            "sim_mcc_mnc":"",
+            "connection_media":"Wifi",
+            "mid": modelid
         },
-        'x1': getX1()
+        'x1': getX1(),
+        "retryType":"0"
     }
     r = requests.post(chkUrl, headers=headers, json=queryjson)
     return r.json()
@@ -72,44 +101,66 @@ def getOTAPkg():
 checkinUrl = 'https://andchin-2.htc.com/htcfotacheckin/rest/checkin'
 checkinUrlCN = 'https://andchin-2.htccomm.com.cn/htcfotacheckin/rest/checkin'
 
-SN = 'CC55TYG07414'
-IMEI = '357290062810490'
+SN = 'FA0611N00326'
+IMEI = '353002100146660'
 timestamp = int(time.time() * 1000)
 
-china = input(">> China Variant? (Y/n): ")
+# china = input(">> China Variant? (Y/n): ")
 
-if china.upper() == 'Y':
-    chkUrl = checkinUrlCN
-else:
-    chkUrl = checkinUrl
+# if china.upper() == 'Y':
+    # chkUrl = checkinUrlCN
+# else:
+    # chkUrl = checkinUrl
+chkUrl = checkinUrl
+
+tid = 0
 
 model = input(">> Model: ")
 version = input(">> Version: ")
 cidnum = input(">> CID: ")
+taskid = input(">> Task id(Can be empty): ")
+modelid = input(">> Model id(Can be empty): ")
 
-#model = 'htc_pmeuhl'
-#version = '2.41.709.73'
-#cidnum = 'HTC__621'
+# Model: htc_rtxspcs
+# Version: 1.04.651.6
+# CID: SPCS_001
+# Taskid: 545803
+# Mid: 2Q6U10000
 
-response = checkin()
 #print(response)
 
-if 'intent' in response:
-    intent = response['intent'][0]
-    if 'data_uri' in intent:
-        uri = intent['data_uri']
-        pkg = intent['pkgFileName']
-        for extra in intent['extra']:
-            if extra['name'] == 'promptSize':
-                size = extra['value']
-                break
-        time_ms = response['time_msec']
-        htc1s = getHTC1S()
-        print('>> FILE: ' + pkg)
-        print('>> SIZE: ' + size)
-        input("Press ENTER to download...")
-        getOTAPkg()
+restart = True
+
+while restart:
+    restart = False
+    
+    response = checkin()
+    if taskid == "" : taskid = 1
+    taskid = int(taskid) + tid
+    print(taskid)
+    
+    if 'intent' in response:
+        intent = response['intent'][0]
+        print(intent)
+        if 'data_uri' in intent:
+            uri = intent['data_uri']
+            pkg = intent['pkgFileName']
+            for extra in intent['extra']:
+                if extra['name'] == 'promptSize':
+                    size = extra['value']
+                    break
+            time_ms = response['time_msec']
+            htc1s = getHTC1S()
+            print('>> FILE: ' + pkg)
+            print('>> SIZE: ' + size)
+            input("Press ENTER to download...")
+            getOTAPkg()
+            restart = False
+        else:
+            tid = tid + 1
+            print(tid)
+            restart = True
+            time.sleep(5)
+            #print('ERROR!!!')
     else:
         print('ERROR!!!')
-else:
-    print('ERROR!!!')
